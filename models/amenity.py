06634ca7a3adf4f0595f582b812a763562844040
abdulrhim4 <abdulrhimsaeed@gmail.com>
2024-03-10 17:00:10 -0700

    AirBnb

---
+++ models/amenity.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+"""classes that inherit from BaseModel:"""
+class State(BaseModel):
+    """State class"""
+
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Write all those classes that inherit from"""
+from models.base_model import BaseModel
+class State(BaseModel):
+    """classes that inherit from BaseModel:"""
+    name = ""
